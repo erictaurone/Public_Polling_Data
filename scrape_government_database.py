c3db313d938c05a763b054fb3d6433679afb35ee
erictaurone <45673519+erictaurone@users.noreply.github.com>
2021-01-20 15:32:50 -0700

    added the database connection to the Heroku database Main next step is to determine the optimal database formatting. I'm thinking it'll go as follows: 1) Main table: houses specific information associated with a given petition 2) Region-specific table: Houses all of the region-specific information, will be connected to Main table via Foreign key 3) Statistics-table: Houses the statistical information for the datasets, connects to Main table via a foreign key 4) Scraping-data table: Houses the web-scraping information for the table

--- scrape_government_database.py
+++ scrape_government_database.py
@@ -3,12 +3,29 @@ import bs4
 import json
 import pandas as pd
 import numpy as np
-
+import psycopg2
+# %%
+# I found it easier to just use the .csv link for all petitions
 csv_link = 'https://petition.parliament.uk/petitions.csv'
+
+# reading the bulk petitions and filtering it for only the 'open' petitions -- data was too massive
+# to go through all of the available information
 bulk_petition_data = pd.read_csv(csv_link)
 bulk_petition_data = bulk_petition_data[bulk_petition_data['State'] == 'open'].reset_index(drop=True)
 
+
 # %%
+# this is the section to connect to Postgresql database
+conn = psycopg2.connect(
+    dbname='d7i9p9ekakqj4i',
+    user='tnoeifjjdvhgnb',
+    password='0bda6e63547e198aec6dd67d9e45c1bffde267a688ce53551550b72eda92d99b',
+    host='ec2-54-237-135-248.compute-1.amazonaws.com',
+    port='5432'
+)
+
+# %%
+# creating a dict for the bulk json data
 json_bulk_data = {}
 for k, row in bulk_petition_data.iterrows():
     split_url = row['URL'].split('/')
@@ -18,4 +35,5 @@ for k, row in bulk_petition_data.iterrows():
     json_bulk_data[str(split_url[-1])] = json_data
     break
 
-
+# %%
+data = pd.DataFrame(json_data['data']['attributes']['signatures_by_constituency'])
